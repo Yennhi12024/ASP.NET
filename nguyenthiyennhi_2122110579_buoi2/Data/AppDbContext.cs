c021658fc8815463b3a5c87f42982fd4103d5dfc
NguyenThiYenNhi <yennhiii12092004@gmail.com>
2025-03-28 20:27:12 +0700

    bài tập buổi 2

---
+++ nguyenthiyennhi_2122110579_buoi2/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nguyenthiyennhi_2122110579_buoi2.Model;
+
+namespace nguyenthiyennhi_2122110579_buoi2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ nguyenthiyennhi_2122110579_buoi2/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace nguyenthiyennhi_2122110579_buoi2.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Price { get; set; }
+    }
+}
