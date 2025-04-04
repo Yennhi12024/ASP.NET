b3ead566839ccc718833ba39f0e312bec4ba2597
NguyenThiYenNhi <yennhiii12092004@gmail.com>
2025-04-04 15:31:56 +0700

    bai buoi 03

---
+++ Nguyenthiyennhi_buoi03/Model/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Nguyenthiyennhi_buoi03.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
+
