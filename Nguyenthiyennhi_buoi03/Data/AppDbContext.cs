eaa03cb9f46fa6b2e0e34ebc6de45cb65fd1ed0d
NguyenThiYenNhi <yennhiii12092004@gmail.com>
2025-04-07 19:18:36 +0700

    Nop Bai Buoi03

---
+++ Nguyenthiyennhi_buoi03/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nguyenthiyennhi_buoi03.Model;
+
+namespace Nguyenthiyennhi_buoi03.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
